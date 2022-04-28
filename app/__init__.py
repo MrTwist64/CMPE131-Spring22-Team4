cd9ec80010cc57b00afdfccae75639ed0bef7f37
Anh Thu <anhthu.pham@sjsu.edu>
2022-04-27 19:20:51 -0700

    Initialized the database as db

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 myobj = Flask(__name__)
 myobj.config.from_mapping(SECRET_KEY='password123')
 
-from app import routes
+db = SQLAlchemy(myapp_obj)
+from app import routes, models
