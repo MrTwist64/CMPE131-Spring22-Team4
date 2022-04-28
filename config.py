d150cac8cc7858e3f018d9ee99699d63a47fd897
Anh Thu <anhthu.pham@sjsu.edu>
2022-04-27 19:18:43 -0700

    Update the config to set location of database

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+#Sets a path to the database
+app.config.from_mapping(
+    SECRET_KEY = ‘you-will-never-guess',
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db'),
+    SQLALCHEMY_TRACK_MODIFICATIONS = False)
